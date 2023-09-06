639d91f81f23fb97b6db4bc5d06a61a27a3302c1
Diego Yael Vargas Becerra <yeyox1945@gmail.com>
2023-09-06 12:01:36 -0600

    - Added pagination dto in common to be reusable.

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PokemonModule, CommonModule],
 })
-export class SeedModule {}
+export class SeedModule { }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) { }
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    await this.pokemonModel.deleteMany({}); // Delete * from Pokemons.
+
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-
+      pokemonToInsert.push({ name, no });
     });
 
-    return data.results;
+    this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
   }
 }
